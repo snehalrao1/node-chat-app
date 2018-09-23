cdd067224ff1a0c9da466cafdbeddd375d5fbd7f
Snehal <snehalrao1@gmail.com>
2018-09-23 08:38:36 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 const path = require('path');
+//const http = require('http');
+
 const express = require('express');
+//const socketIO = require('socket.io');
+//socketio is for establishing a connection between the client and the server. A socketIO
+//needs to be created at both the client and server for communication
+
+//In addition by doing this a list of functions are now available for use at client side
+//for communicating with the server - at /socket.io/socket.io.js
+
 //the below join helps us in directly reaching the public folder. Path is an inbuilt module in nodejs
 var publicPath = path.join(__dirname,'../public');
 
@@ -13,9 +22,27 @@ const port = process.env.PORT || 3000;
 console.log( publicPath);
 
 var app = express();
+//var server = http.createServer(app);
+//using http server instead of express server
+
 //for uploading static files on the server
 app.use(express.static(publicPath));
-//listening to the ebserver at localhost:3000 port
-app.listen(port,()=>{
+
+//var io = socketIO(server);
+//passing the server that will be used by the socket. This socket can now create connections
+/*
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+
+  socket.on('disconnect',()=>{
+    console.log('User disconnected');
+  });
+
+});*/
+//this allows us to listen to events. One of the popular ones is 'connection' whenever a new connection is created
+
+
+//listening to the webserver at localhost:3000 port
+server.listen(port,()=>{
   console.log(`server is up and running on port ${port}`);
 });
